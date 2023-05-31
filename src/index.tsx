34c0a31c1a8a59945c5e11da30049f4a4c9a27c0
Roman Kornev <kornev.roma62@gmail.com>
2023-05-31 16:38:39 +0300

    fix: change structure follders by sliced fature design

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from "react-dom";
-import { App } from "./App";
-import { ThemeProvider } from "./theme/ThemeProvider";
+import { App } from "app/App";
+import { ThemeProvider } from "app/providers/ThemeProvider";
 
 render(
   <ThemeProvider>
